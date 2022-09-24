9f3d8409dbd7897b41ab2adcfee7b0cba8baf901
umzr <lamshishing@gmail.com>
2022-09-24 18:15:46 +0800

    fix: ...

--- cvtest.py
+++ cvtest.py
@@ -1,2 +1,9 @@
 import cv2
+import torch
 print(cv2.__version__)
+
+print(torch.cuda.empty_cache() )
+print(torch.cuda.is_available() )
+print(torch.cuda.device_count() )
+print(torch.cuda.get_device_name(0) )
+print(torch.cuda.memory_reserved)
